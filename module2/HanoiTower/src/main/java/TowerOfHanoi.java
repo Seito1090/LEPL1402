429e02a7d7d70438bab3f56ddc40d309db59edb2
Seito1090 <77192288+Seito1090@users.noreply.github.com>
2022-11-09 15:42:03 +0100

    hanoi worky worky :)

--- module2/HanoiTower/src/main/java/TowerOfHanoi.java
+++ module2/HanoiTower/src/main/java/TowerOfHanoi.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Stack;
 
 public class TowerOfHanoi{
@@ -16,18 +17,19 @@ public class TowerOfHanoi{
         if (n == 1) {
             c.push(a.pop());
         } else {
-            towerOfHanoi(n - 1, a, b, c);
+            towerOfHanoi(n - 1, a, c, b);
             c.push(a.pop());
-            towerOfHanoi(n - 1, b, c, a);
+            towerOfHanoi(n - 1, b, a, c);
         }
     }
-    public static void towerOfHanoiString(int n, String start, String end, String auxiliary) {
+    public static void towerOfHanoiString(int n, String start, String auxiliary, String end) {
         if (n == 1) {
             System.out.println(start + " -> " + end);
         } else {
-            towerOfHanoiString(n - 1, start, auxiliary, end);
+            towerOfHanoiString(n - 1, start, end, auxiliary);
             System.out.println(start + " -> " + end);
-            towerOfHanoiString(n - 1, auxiliary, end, start);
+            towerOfHanoiString(n - 1, auxiliary, start, end);
         }
+
     }
 }
