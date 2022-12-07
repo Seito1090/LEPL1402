249ba0055ca514c69c6a25687583dcb207b7c7c8
Seito1090 <sktpiko@gmail.com>
2022-12-07 15:24:19 +0100

    kinda working

--- module5/LEPL1402_Generics3/src/main/java/Evaluator.java
+++ module5/LEPL1402_Generics3/src/main/java/Evaluator.java
@@ -1,5 +1,4 @@
 import java.util.HashMap;
-import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -7,26 +6,34 @@ public class Evaluator {
 
     public BiFunction<Boolean, Boolean, Boolean> xor_gate() {
         // TODO BY STUDENT
+        return (a,b) -> a^b;
     }
 
     public BiFunction<Boolean, Boolean, Boolean> or_gate() {
         // TODO BY STUDENT
+        return (a,b) -> a || b;
     }
 
     public BiFunction<Boolean, Boolean, Boolean> and_gate() {
         // TODO BY STUDENT
+        return (a,b) -> a && b;
     }
 
     public Function<Boolean, Boolean> not_gate() {
         // TODO BY STUDENT
+        return (a) -> !a;
     }
 
     // Should return a map containing the computation's results (use HashMap)
     // You're asked to store the results under the following keys: "SUM" & "carry_out"
     // TODO WARNING : ONLY USE the previously defined methods to compute the result
     // (INGInious will prevent you from cheating by directly invoking logical operators)
-    public Map<String, Boolean> evaluate_circuit(Boolean a, Boolean b, Boolean carry_in) {
+    public HashMap<String, Boolean> evaluate_circuit(Boolean a, Boolean b, Boolean carry_in) {
         // TODO BY STUDENT
+        HashMap<String, Boolean> result = new HashMap<>();
+        boolean resultXOR = xor_gate().apply(a, b);
+        result.put("SUM", xor_gate().apply(resultXOR, carry_in));
+        result.put("CarryOut", or_gate().apply(and_gate().apply(resultXOR, carry_in), and_gate().apply(a, b)));
+        return result;
     }
-
 }
